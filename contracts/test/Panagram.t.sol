e2d831bc75624ffc8e87e7f600b58679ed56053f
ayushn2 <iayush.n2@gmail.com>
2025-09-22 23:12:36 +0530

    feat: Add TypeScript proof generation and Solidity test helper for zk-proof integration

--- contracts/test/Panagram.t.sol
+++ contracts/test/Panagram.t.sol
@@ -9,6 +9,8 @@ contract PanagramTest is Test{
    
    HonkVerifier public verifier;
    Panagram public panagram;
+   address public constant PLAYER1 = address(0x1);
+   uint256 constant FIELD_MODULUS  = 21888242871839275222246405745257275088548364400416034343698204186575808495617; //prime field order
     
     function setUp() public{
         // deploy the verifier
@@ -17,13 +19,34 @@ contract PanagramTest is Test{
         // deploy the panagram contract
         panagram = new Panagram(verifier);
 
-        // create the answer
-        // start a new round
 
+        // create the answer
+        bytes32 ANSWER = bytes32(uint256(keccak256("ANSWER")) % FIELD_MODULUS);
+
+        // start a new round
+        panagram.newRound(ANSWER);
         
     }
 
+    function _getProof(bytes32 guess, bytes32 correctAnswer) internal returns (bytes memory _proof){
+        uint256 NUM_ARGS = 5;
+        string[] memory inputs = new string[](NUM_ARGS);
+        inputs[0] = "npx";
+        inputs[1] = "tsx";
+        inputs[2] = "js-scripts/generate-proof.js";
+        inputs[3] = vm.toString(guess);
+        inputs[4] = vm.toString(correctAnswer);
+
+        bytes memory result = vm.ffi(inputs);
+        return abi.decode(result, (bytes));
+    }
+
     // test someone receives NFT 0 if they guess correctly first
+    function testCorrectFirstGuess() public {
+        vm.prank(PLAYER1);
+        panagram.makeGuess(proof);
+    }
+
     // test someone receive s NFT 1 if they guess correctly second
     // test we can start a new round after time has passed and there was a winner
 
